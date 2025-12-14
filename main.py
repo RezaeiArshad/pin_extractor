ec890fa77220fa95abc494cce523a75861e08d2c
RezaeiArshad <mehdirezaeiarshad@gmail.com>
2025-12-14 22:45:06 +0330

    I have to go study , I'll comeback to this tomorrow

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def message_finder(text, pattern='diagnal', mode='first_letter'):
     secret_message = []
-    lines = text.split('\n')
+    lines = text.split('\n')    
     if pattern == 'diagnal':
        for index_of_line, line in enumerate(lines):
-        words = line.split(' ')
+        words = line.split(' ') 
         if len(words) > index_of_line:
             if mode == 'first_letter':
                secret_message.append(words[index_of_line][0])
@@ -11,22 +11,39 @@ def message_finder(text, pattern='diagnal', mode='first_letter'):
                secret_message.append(words[index_of_line][-1])
 
     elif pattern == 'reverse_diagnal':
-        last_index = len(lines[0].split(' ')) - 2
-
-        for index_of_line, line in enumerate(lines):
-            words = line.split(" ")           
+        last_index = -1
+        for line in lines:  
+            words = line.split(' ')        
             if words[last_index]:
                 if mode == 'first_letter':
                     secret_message.append(words[last_index][0])
                 elif mode == 'last_letter':
                     secret_message.append(words[last_index][-1])
             last_index -= 1
+    elif pattern == 'zigzag':
+        last_index = -1        
+        for index_of_line, line in enumerate(lines):
+            words = line.split(' ') 
+            if index_of_line % 2 == 0:
+                if len(words) > index_of_line:
+                    if mode == 'first_letter':
+                        secret_message.append(words[index_of_line][0])
+                    elif mode == 'last_letter':                    
+                        secret_message.append(words[index_of_line][-1]) 
+            elif index_of_line % 2 == 1:
+                for line in lines:
+                   if words[last_index]:
+                        if mode == 'first_letter':
+                            # this part is running twice
+                            secret_message.append(words[last_index][0])
+                        elif mode == 'last_letter':
+                            secret_message.append(words[last_index][-1])
+                last_index -= 1
 
-        
         
     return ''.join(secret_message)
 
 
 print(message_finder('hello\nand interesting', 'diagnal', 'first_letter'))
-    
-print(message_finder('tell him\n turph i'))
+print(message_finder('abduct\nwhen truth dies\nalibi wont talk\nmess to be made', 'reverse_diagnal', 'last_letter'))
+print(message_finder("well I'm out\nsee you maybe", 'zigzag', 'first_letter'))
